9b7feaf93f9f8627b9f9857249b23b4123c18e23
akm-09 <nebula.01.01.1996@gmail.com>
2022-08-02 20:42:54 +0500

    index.js faylga try catch qo'shildi...

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const loopTests = require('./test/test');
+
+function getTime(startedTime) {
+    return Date.now() - startedTime;
+}
+
+try {
+    let startedTime = Date.now();
+    
+} catch (error) {
+    console.error(error);
+}

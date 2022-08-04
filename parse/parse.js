fff9987ed06e46c8bd350183ef2d9d5f891f9ca3
hayitboyevkh <hayitboyev2309x@gmail.com>
2022-08-04 10:41:38 +0500

    parse.js is updated

--- parse/parse.js
+++ parse/parse.js
@@ -1,5 +1,9 @@
 const { loopsPath } = require('../test/test');
 
 module.exports = (normalized_Filename) => {
-    console.log(loopsPath.includes(normalized_Filename));
+    try {
+        loopsPath.includes(normalized_Filename)
+    } catch (error) {
+        console.log(error);
+    }
 }
